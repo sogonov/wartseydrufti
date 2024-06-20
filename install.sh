930307e406fc3117cec06bd45523193fca7e3227
sogonov <sogonove@yandex.ru>
2024-06-20 11:21:41 +0400

    add title
    add description

---
+++ install.sh
@@ -0,0 +1,8 @@
+# URL файла для скачивания
+URL="https://github.com/sogonov/wartseydrufti/raw/main/TotalKlipper.tar.gz"
+
+# Команда для скачивания файла
+wget $URL
+
+# Сообщение об успешном выполнении
+echo "Файл успешно скачан."
